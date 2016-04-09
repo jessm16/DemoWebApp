612547904e44f1e0ec2c95d06bf240b128b3ef97
Jessica <merritt.j@husky.neu.edu>
2016-04-09 16:52:45 -0400

    the encyclopedia

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-  getWeather();
+  getCharacter();
 });
 
 function searchCharacter() {
   var searchQuery = $('.search').val(); // grab value from search input
-  getWeather(searchQuery);
+  getCharacter(searchQuery);
 }
 
-function getWeather(searchQuery) {
+function getCharacter(searchQuery) {
   var url = 'http://gateway.marvel.com:80/v1/public/characters?'; // url for the API
   var params = {
    apikey: apiKey, // coming from apiKey in index.ejs
@@ -21,10 +21,14 @@ function getWeather(searchQuery) {
     success: function (data) {
       var description = data.data.results[0].description;
       var name = data.data.results[0].name;
-      $('.name').text(${name});
+      var link = data.data.results[0].resourceURI;
+      $('.name').text(name);
+      $('.summary').text(description);
+      $('.link').text(link);
     },
     error: function (error) {
       $('.error-message').text('An error occurred :(');
     }
   });
 }
+
